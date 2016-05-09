2cb93132536dabc51471768bcf1216de638dda34
Chyroc <chen_yunpeng@foxmail.com>
2016-05-09 19:43:37 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,93 @@
+    # 基于搜狗微信搜索的微信公众号采集
+
+标签（空格分隔）： 微信 公众号 python 搜狗 爬虫
+
+---
+
+# 使用
+    import wechatsogou as wechats
+
+# 搜索公众号 - search_gzh_info
+## 使用
+
+    infos = search_gzh_info(name, page)
+    for info in infos:
+        name = info['name']
+## 返回
+返回结果是列表，每一项均是公众号具体信息字典，具体如下
+字段|含义
+-|-
+name|公众号名称
+wechatid|公众号ID
+jieshao|介绍
+renzhen|认证信息，为空表示未认证
+qrcode|二维码图片地址
+img|头像地址
+url|最近文章地址
+
+# 获取公众号
+## 使用
+对于一个已知`wechatid`的公众号，如果需要获取其最近文章，可以通过以下方式先获取具体信息（包括最近文章地址），然后根据``获取
+
+    detail = get_gzh_info(wechatid）
+    url = detail['url']
+## 返回
+返回结果与上述search_gzh_info函数返回结果一致
+
+# 获取最近文章列表字典 - get_gzh_article_dict
+
+    msgdict = get_gzh_article_dict(url)
+msgdict是字典
+字段|含义
+-|-
+list|只有这一个字段，包含具体信息
+
+
+    infos = msgdict['list']
+    for info in infos:
+        comm_msg_info = info['comm_msg_info']
+        app_msg_ext_info = info['app_msg_ext_info']
+
+comm_msg_info是字典
+字段|含义
+-|-
+status|...
+fakeid|...
+datetime|...
+type|...
+id|...
+content|...
+
+app_msg_ext_info也是字典
+字段|含义
+-|-
+subtype|...
+title|标题
+copyright_stat|文章类型（100为普通，11原创，101转载）
+author|作者
+source_url|原文地址
+fileid|...
+content|...
+content_url|文章地址
+cover|封面图片地址
+digest|描述
+is_multi|是否多图文（1位多图文，有multi_app_msg_item_list字段，0为单图文）
+multi_app_msg_item_list|多图文
+
+# 获取最近文章列表 - get_gzh_article_detail
+
+    msgdict = get_gzh_article_dict(url)
+    item = get_gzh_article_detail(msgdict)
+item是列表，每一项均是具体信息字典
+字段|含义
+-|-
+title|标题
+digest|描述
+cover|封面图片地址
+main|是否为一次推送中第一篇文章
+content_url|文章地址
+author|作者
+copyright_stat|文章类型（100为普通，11原创，101转载）
+source_url|原文地址
+fileid|...
+content|...
---
+++ wechatsogou.py
@@ -0,0 +1,164 @@
+#coding=utf-8
+
+import os,sys
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir)
+
+from tools import myproxy as proxy
+from tools import myrequests as req
+from lxml import etree
+import requests
+import random
+import re
+
+def get_elem_text(elem):
+    rc = []
+    for node in elem.itertext():
+        rc.append(node.strip())
+    return ''.join(rc)
+
+def get_encoding_from_reponse(r):
+    encoding = requests.utils.get_encodings_from_content(r.text)
+    if encoding:
+        return encoding[0]
+    else:
+        return requests.utils.get_encoding_from_headers(r.headers)
+def _get(url, host=''):
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.101 Chrome/45.0.2454.101 Safari/537.36',
+        "Referer": 'http://weixin.sogou.com/',
+        'Host': host if host else 'weixin.sogou.com',
+    }
+    # pro = proxy.run_one()
+    # proxies = {
+    #     pro['http']: "http://" + pro['ip'] + ":" + pro['duan']
+    # }
+    # print(proxies)
+    r = requests.get(url, headers=headers)  # , proxies=proxies
+    if r.status_code == requests.codes.ok:
+        r.encoding = get_encoding_from_reponse(r)
+        if '需要您协助验证' in r.text:
+            raise Exception('http get Verification code')
+    else:
+        raise Exception('requests status_code error')
+    return r.text
+def replace_html(s):
+    s = s.replace('&quot;','"')
+    s = s.replace('&amp;','&')
+    s = s.replace('amp;','')
+    s = s.replace('&lt;','<')
+    s = s.replace('&gt;','>')
+    s = s.replace('&nbsp;',' ')
+    s = s.replace(r"\\", r'')
+    return s
+def _search_gzh(name, page=1):
+    request_url = 'http://weixin.sogou.com/weixin?query='+name+'&_sug_type_=&_sug_=n&type=1&page='+str(page)+'&ie=utf8'
+    try:
+        text = _get(request_url)
+        return text
+    except Exception as e:
+        print(e)
+        return False
+
+def search_gzh_info(name, page):
+    text = _search_gzh(name, page)
+    page = etree.HTML(text)
+
+    info_imgs = page.xpath(u"//div[@class='img-box']/img")
+    img = list()
+    for info_img in info_imgs:
+        img.append(info_img.attrib['src'])
+
+    info_urls = page.xpath(u"//div[@target='_blank']")
+    url = list()
+    for info_url in info_urls:
+        url.append(info_url.attrib['href'])
+
+    name = list()
+    wechatid = list()
+    jieshao = list()
+    renzhen = list()
+    info_instructions = page.xpath(u"//div[@class='txt-box']")
+    for info_instruction in info_instructions:
+        cache = get_elem_text(info_instruction)
+        cache = cache.replace('red_beg','')
+        cache = cache.replace('red_end', '')
+        cache_list = cache.split('\n')
+        cache_re = re.split('微信号：|功能介绍：|认证：|最近文章：', cache_list[0])
+        name.append(cache_re[0])
+        wechatid.append(cache_re[1])
+        if ".authnamewrite('2')" in cache_re[2]:
+            jieshao.append(cache_re[2].replace(".authnamewrite('2')",''))
+            renzhen.append(cache_re[3])
+        else:
+            jieshao.append(cache_re[2])
+            renzhen.append('')
+
+    qrcodes = list()
+    info_qrcodes = page.xpath(u"//div[@class='pos-ico']/div/img")
+    for info_qrcode in info_qrcodes:
+        qrcodes.append(info_qrcode.attrib['src'])
+
+    returns = list()
+    for i in range(len(qrcodes)):
+        returns.append(
+            {'name':name[i],
+             'wechatid':wechatid[i],
+             'jieshao':jieshao[i],
+             'renzhen':renzhen[i],
+             'qrcode':qrcodes[i],
+             'img':img[i],
+             'url':url[i]
+             }
+        )
+    return returns
+
+def get_gzh_info(wechatid):
+    info = search_gzh_info(wechatid, 1)
+    if info:
+        return info[0]
+    else:
+        return False
+
+def get_gzh_article_dict(url):
+    try:
+        text = _get(url, 'mp.weixin.qq.com')
+        print('dui')
+    except Exception as e:
+        print(e)
+        exit()
+    msglist = re.findall("var msgList = '(.+?)';", text, re.S)[0]
+    msgdict = eval(replace_html(msglist))
+    return msgdict
+
+def get_gzh_article_detail(msgdict):
+    """
+    省略了list->comm_msg_info,list->app_msg_ext_info->(subtype)
+    """
+    items = list()
+    for listdic in msgdict['list']:
+        item = listdic['app_msg_ext_info']
+        """copyright_stat解释
+        100是普通
+        11是原创
+        101是转载
+        """
+        items.append(
+            {'main':'1','title': item['title'], 'digest': item['digest'], 'content': item['content'], 'fileid': item['fileid'],
+             'content_url': 'http://mp.weixin.qq.com' + item['content_url'], 'source_url': item['source_url'],
+             'cover': item['cover'], 'author': item['author'], 'copyright_stat': item['copyright_stat']})
+        if item['is_multi'] == 1:
+            for multidic in item['multi_app_msg_item_list']:
+                items.append({'main':'0','title': multidic['title'], 'digest': multidic['digest'], 'content': multidic['content'],
+                              'fileid': multidic['fileid'],
+                              'content_url': 'http://mp.weixin.qq.com' + multidic['content_url'],
+                              'source_url': multidic['source_url'],
+                              'cover': multidic['cover'], 'author': multidic['author'],
+                              'copyright_stat': multidic['copyright_stat']})
+
+    return items
+
+msgdict = get_gzh_article_dict(
+    'http://mp.weixin.qq.com/profile?src=3&timestamp=1462779952&ver=1&signature=OpcTZp20TUdKHjSqWh7m73RWBIzwYwINpib2ZktBkLF9AWOMZksuvnJtrqsb98P113QJ*9pWPeKn6N-JaG*yKg==')
+item = get_gzh_article_detail(msgdict)
+print(item)
