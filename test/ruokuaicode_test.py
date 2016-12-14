cbf7be54e34ce8027eaea186563700780a36b73f
Chyroc Chen <Chyroc@chenyunpengs-MacBook-Pro.local>
2016-12-15 00:31:06 +0800

    添加若快打码的测试

---
+++ test/ruokuaicode_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from test import config
+from wechatsogou.ruokuaicode import RClient
+
+
+class RuokuaicodeTest(unittest.TestCase):
+    def test_create(self):
+        rc = RClient(config.username, config.password, config.soft_id, config.soft_key)
+        with open('code.jpg', 'rb') as f:
+            im = f.read()
+        result = rc.create(im, '3060')
+        result_str = result['Result'].lower()
+        assert result_str == '61xmv3'
+
+                      ∫
+if __name__ == '__main__':
+    unittest.main()
