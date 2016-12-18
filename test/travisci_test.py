eec069f73f650f7e0f4d8451cbd1915f94c7bb27
Chyroc Chen <Chyroc@chenyunpengs-MacBook-Pro.local>
2016-12-18 21:06:40 +0800

    add Environment Variables test 10

--- test/travisci_test.py
+++ test/travisci_test.py
@@ -1,33 +1,9 @@
+import sys
+
+if sys.argv[1] == 'true':
+    print('ci env')
 
 
-
-
-try:
-    print(1)
-    print(ruokuai_name)
-except Exception as e:
-    print(e)
-
-try:
-    print(2)
-    import sys
-    print(sys.argv)
-except Exception as e:
-    print(e)
-
-
-try:
-    print(3)
-    import System
-    print(System.getenv())
-except Exception as e:
-    print(e)
-
-
-try:
-    print(4)
-    import os
-    print(os.environ[''])
-    print(os.environ.get('ruokuai_name'))
-except Exception as e:
-    print(e)
+from test import config
+if config.soft_key:
+    print('local env')
