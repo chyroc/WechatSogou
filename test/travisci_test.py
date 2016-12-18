a0207e52f03fc8272d585958dff81cb600248e71
Chyroc Chen <Chyroc@chenyunpengs-MacBook-Pro.local>
2016-12-18 20:12:45 +0800

    add Environment Variables test 2

--- test/travisci_test.py
+++ test/travisci_test.py
@@ -1,1 +1,26 @@
-print(ruokuai_name)
+
+
+
+
+try:
+    print(1)
+    print(ruokuai_name)
+except Exception as e:
+    pass
+
+try:
+    print(2)
+    import sys
+    print(sys.argv)
+except Exception as e:
+    pass
+
+
+try:
+    print(3)
+    import System
+    System.getenv()
+except Exception as e:
+    pass
+
+
