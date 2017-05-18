2aab2e3721fdbfbcd60bc8046916f3c71aefddf4
Chyroc <chyroc@madas-MacBook-Pro.local>
2017-05-18 10:26:41 +0800

    add list_or_empty test

--- test/TestTools.py
+++ test/TestTools.py
@@ -1,8 +1,11 @@
 import unittest
 from nose.tools import assert_raises, assert_equal
 
+from lxml import etree
 from wechatsogou.tools import (
-    list_or_empty
+    list_or_empty,
+    get_elem_text
+
 )
 
 
@@ -16,6 +19,16 @@ class TestTools(unittest.TestCase):
         assert_equal(list_or_empty([], int), 0)
         assert_equal(list_or_empty([], str), '')
         assert_equal(list_or_empty([], list), [])
+
+    def test_get_elem_text(self):
+        html = '''
+        <div>
+            <div>111</div>
+            <div>222</div>
+        </div>
+        '''
+        elem = etree.HTML(html)
+        assert_equal(get_elem_text(elem), '111222')
 
 
 if __name__ == '__main__':
