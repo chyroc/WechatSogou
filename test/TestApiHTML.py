88dffbb073fe3734a5d4487adcbdb6cdfc131dd4
Chyroc <chen_yunpeng@foxmail.com>
2017-07-22 13:27:49 +0800

    tmp

---
+++ test/TestApiHTML.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestHTML(unittest.TestCase):
+    @unittest.skip
+    def test_html(self):
+        pass
+        # TODO 比对网页和GIT文件
+
+
+if __name__ == '__main__':
+    unittest.main()
