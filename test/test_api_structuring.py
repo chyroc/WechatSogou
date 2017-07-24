d5d6e50dbf4f33d5b7f5a884c0af81fd44349140
Chyroc <chen_yunpeng@foxmail.com>
2017-07-24 21:37:51 +0800

    test struct article list (#84)

--- test/test_api_structuring.py
+++ test/test_api_structuring.py
@@ -6,7 +6,7 @@ import io
 import os
 import unittest
 
-from nose.tools import assert_equal, assert_in
+from nose.tools import assert_equal, assert_in, assert_true, assert_greater_equal
 
 from wechatsogou.refactor_structuring import WechatSogouStructuring
 
@@ -38,11 +38,28 @@ class TestStructuringGzh(unittest.TestCase):
             assert_in('mp.weixin.qq.com/rr?src=', gzh['qrcode'])
             assert_in('img01.sogoucdn.com/', gzh['img'])
 
-        assert_equal(['山东高考指南', '高考家长圈', '河南高考指南', '高考360', '云天高考', '腾讯高考', '高考快讯', '专业中高考教育', '晟嘉高考', '新东方在线高考辅导'],
+        assert_equal(['山东高考指南',
+                      '高考家长圈',
+                      '河南高考指南',
+                      '高考360',
+                      '云天高考',
+                      '腾讯高考',
+                      '高考快讯',
+                      '专业中高考教育',
+                      '晟嘉高考',
+                      '新东方在线高考辅导'],
                      names)
-        assert_equal(
-            [u'sdgkzn', u'sinagkjzq', u'hngaokao', u'sctvgaokao360', u'yuntiangaokao', u'qq_gaokao', u'gkkx678',
-             u'gh_591a43050b5f', u'tjsjgk', u'koogaokao'], wechat_ids)
+        assert_equal([u'sdgkzn',
+                      u'sinagkjzq',
+                      u'hngaokao',
+                      u'sctvgaokao360',
+                      u'yuntiangaokao',
+                      u'qq_gaokao',
+                      u'gkkx678',
+                      u'gh_591a43050b5f',
+                      u'tjsjgk',
+                      u'koogaokao'],
+                     wechat_ids)
         assert_equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], post_perms)
         assert_equal(
             ['这里是山东最权威最专业的高考交流平台,由山东商报徐玉芹教育工作室独家运作.本平台与山东商报高考交流群互为依托,为山东考生和家长提供最及时、最准确的高考政策及信息解读,以及一流的填报志愿咨询服务.合作...',
@@ -52,10 +69,88 @@ class TestStructuringGzh(unittest.TestCase):
              '高端教育品牌,高分考生的加油站,重点中学的合作伙伴.开阔考生视野、提升认知,在以“研究”为主线的基础上,将考生培养成一个全面型的人才.课程特色,全科协调、单科精讲.云天高考一直深受高分考生和家长的追...',
              '腾讯高考频道是中国最具互动性高考门户网站.主要为中国高考生及家长提供有价值的资讯和辅导.内容包括:新闻、评论、视频、各科辅导、志愿填报、家长指南等多方面.',
              '高考快讯平台专为考生家长提供最新高考资讯、志愿填报指南、名校排行榜、状元经验、学习方法、高分秘籍等等,我们的努力将伴随着您圆大学梦,欢迎关注阅读!',
-             '旨在做最专业的中高考教育交流平台,第一时间传递权威的中高考资讯,为孩子的未来保驾护航!', '关于天津高考,你关注我们一个就够啦!', '提供高考资讯、高考院校库、在线答疑、政策解读及试题发布.'],
+             '旨在做最专业的中高考教育交流平台,第一时间传递权威的中高考资讯,为孩子的未来保驾护航!',
+             '关于天津高考,你关注我们一个就够啦!',
+             '提供高考资讯、高考院校库、在线答疑、政策解读及试题发布.'],
             introductions)
-        assert_equal(['《山东商报》社', '新浪网技术(中国)有限公司', '郑州新东方培训学校', '四川省电化教育馆(四川教育电视台)', '北京云天共业教育科技有限公司', '深圳市腾讯计算机系统有限公司',
-                      '广州卓越教育培训中心', '大连沙河口科苑文化培训学校', '天津市南开区晟嘉培训中心', '北京新东方迅程网络科技股份有限公司'], authentications)
+        assert_equal(['《山东商报》社',
+                      '新浪网技术(中国)有限公司',
+                      '郑州新东方培训学校',
+                      '四川省电化教育馆(四川教育电视台)',
+                      '北京云天共业教育科技有限公司',
+                      '深圳市腾讯计算机系统有限公司',
+                      '广州卓越教育培训中心',
+                      '大连沙河口科苑文化培训学校',
+                      '天津市南开区晟嘉培训中心',
+                      '北京新东方迅程网络科技股份有限公司'],
+                     authentications)
+
+    def test_structuring_article(self):
+        file_name = '{}/{}'.format(os.getcwd(), 'test/file/search-gaokao-article.html')
+        with io.open(file_name, encoding='utf-8') as f:
+            search_gaokao_article = f.read()
+
+        article_list = WechatSogouStructuring.get_article_by_search(search_gaokao_article)
+
+        titles = []
+        abstracts = []
+        gzh_names = []
+        isvs = []
+        assert_equal(10, len(article_list))
+        for i in article_list:
+            article = i['article']
+            titles.append(article['title'])
+            abstracts.append(article['abstract'])
+
+            assert_in('mp.weixin.qq.com/s?src=3&timestamp=', article['url'])
+            assert_true(isinstance(article['imgs'], list))
+            assert_greater_equal(len(article['imgs']), 1)
+
+            gzh = i['gzh']
+
+            assert_in('mp.weixin.qq.com/profile?src=3&timestamp', gzh['article_list_url'])
+            assert_in('wx.qlogo.cn/mmhead', gzh['headimage'])
+            gzh_names.append(gzh['name'])
+            isvs.append(gzh['isv'])
+
+        # article
+        assert_equal(['高考有多重要,为什么要重视高考?丨微观点',
+                      '高考:穷人考不好,中产考状元,精英不高考',
+                      '关于高考志愿的一点建议,仅供参考!',
+                      '刚刚,高考“满分”诞生了!(附各省高考分数线)',
+                      '高考学霸榜出炉!义乌最高分是她!排名...',
+                      '【高考】权威发布!2017年我省高考各项日程',
+                      '【高考】黑龙江省2017年普通高考成绩即将发布',
+                      '高考2017 | 全国各省区市高考录取时间大汇总,最新最全!',
+                      '高考志愿这么填,等于多考20分!这位特级教师的志愿填报方法很管用!',
+                      '高考填志愿,如何选专业?学长学姐有话说'],
+                     titles)
+        assert_equal(['针对这个问题,其实占豪已经谈过,但还是想借高考之后、借这位小战友的留言,结合自己的人生经验,谈谈个人对这件事的看法....',
+                      '#条条大路通罗马,有人就出生在罗马#前几天北京文科高考状元熊轩昂接受澎湃新闻的采访的时候,说了下面这段话. “农村地区的...',
+                      '最近一直有哥迷留言问,填报高考志愿该选什么专业? 讲真,这个问题很难回答.专业选择没有绝对的好坏对错,跟考试成绩、个人兴...',
+                      '高考会有满分的情况吗?还真有!6月22日开始,全国各省的高考成绩陆续发布.22日晚上,成都市青白江区一个小区内人声鼎沸,因...',
+                      '浙江新高考各类别各段分数线及考生成绩于昨日揭晓.考生可凭考生号、密码查询自己的考试成绩!今年的高考成绩,经浙江省教育考...',
+                      '根据我省招生录取工作安排,现将近期有关高考工作日程公布如下:一、高考成绩公布时间6月24日左右省招考院通过黑龙江省招生考...',
+                      '黑龙江省2017年普通高考成绩即将发布 我省今年高考网上评卷工作现已结束,经过成绩核查、成绩校验等多个环节后,我省高考成绩...',
+                      '2017年高考录取工作开始了,各省区市高考录取工作何时进行?为了方便考生和家长及时了解,小编为大家作了最新最全的梳理.(图...',
+                      '各地高考成绩已陆续公布,在本公众号回复“高考查分”即可查询!~长按二维码即可关注本车~自昨天开始,全国各省份陆续公布...',
+                      '导语高考成绩和批次线已经出来了,想必同学们已经开始进入另一重要环节——志愿填报.你是不是在为选专业而纠结痛苦?不怕!...', ],
+                     abstracts)
+
+        # gzh
+        assert_equal(['占豪',
+                      '才华有限青年',
+                      '新闻哥',
+                      '光明网',
+                      '义乌十八腔',
+                      '龙招港',
+                      '龙招港',
+                      '微言教育',
+                      '高考直通车',
+                      '阳光高考信息平台', ],
+                     gzh_names)
+        assert_in(1, isvs)
+        assert_in(0, isvs)
 
 
 if __name__ == '__main__':
--- wechatsogou/refactor_structuring.py
+++ wechatsogou/refactor_structuring.py
@@ -2,9 +2,11 @@
 
 from __future__ import absolute_import, unicode_literals, print_function
 
+import re
+
 from lxml import etree
 
-from wechatsogou.tools import get_elem_text
+from wechatsogou.tools import get_elem_text, list_or_empty
 
 
 class WechatSogouStructuring(object):
@@ -34,8 +36,70 @@ class WechatSogouStructuring(object):
             })
         return relist
 
-    def get_article_by_search(self, text):
-        pass
+    @staticmethod
+    def get_article_by_search(text):
+        page = etree.HTML(text)
+        lis = page.xpath('//ul[@class="news-list"]/li')
+
+        articles = []
+        for li in lis:
+            url = li.xpath('div[1]/a/@href')
+            if url:
+                title = li.xpath('div[2]/h3/a')
+                imgs = li.xpath('div[1]/a/img/@src')
+                abstract = li.xpath('div[2]/p')
+                time = li.xpath('div[2]/div/span/script/text()')
+                gzh_info = li.xpath('div[2]/div/a')[0]
+            else:
+                url = li.xpath('div/h3/a/@href')
+                title = li.xpath('div/h3/a')
+                imgs = []
+                spans = li.xpath('div/div[1]/a')
+                for span in spans:
+                    img = span.xpath('span/img/@src')
+                    if img:
+                        imgs.append(img)
+                abstract = li.xpath('div/p')
+                time = li.xpath('div/div[2]/span/script/text()')
+                gzh_info = li.xpath('div/div[2]/a')[0]
+
+            if title:
+                title = get_elem_text(title[0]).replace("red_beg", "").replace("red_end", "")
+            else:
+                title = ''
+            if abstract:
+                abstract = get_elem_text(abstract[0]).replace("red_beg", "").replace("red_end", "")
+            else:
+                abstract = ''
+
+            time = list_or_empty(time)
+            time = re.findall('timeConvert\(\'(.*?)\'\)', time)
+            time = list_or_empty(time, int)
+            gzh_article_url = gzh_info.xpath('@href')
+            gzh_headimage = gzh_info.xpath('@data-headimage')
+            # gzh_qrcodeurl = gzh_info.xpath('@data-encqrcodeurl')
+            gzh_name = gzh_info.xpath('text()')  # gzh_info.xpath('@data-sourcename')
+            # gzh_wechatid = gzh_info.xpath('@data-username')
+            gzh_isv = gzh_info.xpath('@data-isv')
+            # gzh_avgpublish = gzh_info.xpath('@data-avgpublish')
+            # gzh_avgread = gzh_info.xpath('@data-avgread')
+
+            articles.append({
+                'article': {
+                    'title': title,
+                    'url': list_or_empty(url),
+                    'imgs': imgs,
+                    'abstract': abstract,
+                    'time': time
+                },
+                'gzh': {
+                    'article_list_url': list_or_empty(gzh_article_url),
+                    'headimage': list_or_empty((gzh_headimage)),
+                    'name': list_or_empty(gzh_name),
+                    'isv': list_or_empty(gzh_isv, int),
+                }
+            })
+        return articles
 
     def get_gzh_by_history(self, text):
         pass
