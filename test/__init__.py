51af2925a291b38681667925ca6a58da30c5b73e
Chyroc <chen_yunpeng@foxmail.com>
2017-07-30 23:33:04 +0800

    fix test file not equal in shell vs ide (fixes 121)

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import, unicode_literals, print_function
 
+import os
+
 from wechatsogou.request import WechatSogouRequest
 from wechatsogou.structuring import WechatSogouStructuring
 
@@ -9,4 +11,4 @@ ws = WechatSogouRequest()
 ws_structuring = WechatSogouStructuring()
 
 gaokao_keyword = '高考'
-fake_data_path = 'test/file'
+fake_data_path = '{}/file'.format(os.getcwd() if 'test' in os.getcwd() else '{}/test'.format(os.getcwd()))
