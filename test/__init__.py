040aaf9915aa4258e375cec4dd9cd6481cce7ad8
Edison.Hsu <xudongdonghome@gmail.com>
2018-10-07 19:08:30 +0800

    fix: get_gzh_article_by_history结果为空时异常的问题 (#212)

--- test/__init__.py
+++ test/__init__.py
@@ -10,5 +10,6 @@ from wechatsogou.structuring import WechatSogouStructuring
 ws = WechatSogouRequest()
 ws_structuring = WechatSogouStructuring()
 
+empty_search_result_keyword = 'gggggggggggggggggg'
 gaokao_keyword = '高考'
 fake_data_path = '{}/file'.format(os.getcwd() if 'test' in os.getcwd() else '{}/test'.format(os.getcwd()))
